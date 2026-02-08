0c67157860716355caeb835feaf68d912a285ae6
Bharathi-ravi2014 <bharathiravi2014@gmail.com>
2026-02-08 12:46:30 +0530

    Add files via upload
    Employee Details assignment

---
+++ EmployeeDetails.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name= "John Doe";
+int age= 30;
+double salary= 55000.75;
+String gender = "M";
+boolean acceptTerms= true;
+float rating= 4.5f;
+String mobileNumber="9876543210";
+int pinCode= 56001;
+int luckyNumber= 7;
+System.out.println(name+"\n"+age+"\n"+salary+"\n"+gender+"\n"+acceptTerms+"\n"+rating+"\n"+mobileNumber+"\n"+pinCode+"\n"+luckyNumber+"\n");
+	}
+
+}
