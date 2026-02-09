4baea1c37504f2160b94142d172cdc46f96a28d9
Bharathi-ravi2014 <bharathiravi2014@gmail.com>
2026-02-09 21:33:38 +0530

    Add files via upload
    Library assignment

---
+++ Library.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Library {
+	
+	public void addBook() {
+		System.out.println("Book added successfully");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
